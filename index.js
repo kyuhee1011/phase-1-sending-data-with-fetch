9b5b6813ca3471c43632ec8e3dc588f037dc9ad6
Kyuhee Lee <kyuhee.lee.1011@gmail.com>
2023-04-13 15:48:22 -0400

    sending data lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+
+
+
+  function submitData (name, email) {
+  return fetch('http://localhost:3000/users',{ 
+    method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  },
+  body: JSON.stringify({
+    name:name,
+    email: email,
+  }),
+
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      document.body.innerHTML = object.id;
+    })
+
+      .catch(function (error) {
+        alert("Oops, wrong information");
+        console.log(error.message);
+        const p = document.createElement('p')
+        p.innerHTML = error.message
+        document.body.appendChild(p)
+    });
+}
